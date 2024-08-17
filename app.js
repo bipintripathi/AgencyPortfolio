5bb43f2b7f592e6277ce0c9732e53c2bd028e682
Subhajit <subhajit7252@gmail.com>
2024-08-17 13:28:11 +0530

    layout fixes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = 3000;
@@ -12,7 +13,8 @@ const PORT = 3000;
 // ejs config
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-
+app.use(expressLayouts);
+app.set('layout', 'layout');
 // add website routes
 app.use('/', require('./routes/website'))
 
